b755c030c5a86ee653eb6f7fd9a59a4390b6b719
Tony Guntharp <fusion94@gmail.com>
2026-03-19 16:43:18 -0500

    style: dark theme matching original Norlin aesthetic
    - Dark navy background (#0f0e15), coral accent (#ff7b7b)
    - Inter font, 900 weight headings
    - Header: site title + nav + social icons (GitHub, X)
    - Footer: copyright, DamageLabs link, social links
    - BaseHead: Umami analytics, removed old Atkinson fonts
    - Consistent with current fusion94.org look
    
    Refs #1

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'fusion94.org';
+export const SITE_DESCRIPTION = 'Totally Random Musings from the Edge';
